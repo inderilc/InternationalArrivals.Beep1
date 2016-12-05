ccea69ee13f9d0a9fdfc09309d9b1362a6f7b65d
inderilc <inder@israellopezconsulting.com>
2016-12-05 23:43:11 +1000

    Commiting code

--- InternationalArrivals.Beep1/Class1.cs
+++ InternationalArrivals.Beep1/Class1.cs
@@ -1,12 +1,185 @@
-﻿using System;
+﻿using Beep1.Plugin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Beep1Common.Utils;
+using Beep1.MenuBase;
+using Beep1.Utils;
+using FishbowlSDK;
+using Beep1.Objects;
+using System.IO;
 
 namespace InternationalArrivals.Beep1
 {
-    public class Class1
+    public class IACycle : CustomerController
     {
+        private ItemCache ic;
+
+        public IACycle(GlobalResources res) : base(res)
+        {
+            api = res.api;
+            db = res.db;
+            ic = res.ItemCache;
+        }
+
+        public override bool HasAccess()
+        {
+            return true;
+        }
+
+        public override bool IsCompatible()
+        {
+            return true;
+        }
+
+        public override MenuOptions PublicMenuOption()
+        {
+            return new MenuOptions("IA Cycle", "IAC", 99, this);
+        }
+
+        public override void Run()
+        {
+            bool cancel = false;
+            while (!cancel)
+            {
+                C.CL();
+                CycleCount(out cancel);
+            }
+
+        }
+        private void CycleCount(out bool cancel)
+        {
+            CycleLineItem item = new CycleLineItem();
+            cancel = false;
+            bool finish = false;
+            Location location = null;
+            String partNo = null;
+            int qty = 0;
+            Dictionary<String, int> PartList = new Dictionary<string, int>();
+            while (!cancel && !finish)
+            {
+                bool isReady = location != null && !String.IsNullOrEmpty(partNo) && qty > 0;
+                C.CL();
+                C.WL("Cycle Count");
+                C.WL("Location: " + (location?.Name ?? "NONE"));
+                C.WL("Part: " + (partNo ?? "None"));
+                C.WL("Qty: " + ((qty > 0) ? qty : 0));
+
+                if (location == null)
+                {
+                    C.WL("Location:");
+                    location = LocationUtils.captureDisplayLocation(api, C.getString(out cancel));
+                    if (cancel)
+                        continue;
+                }
+                if (location != null)
+                {
+                    ConsoleKey? fnKey;
+                    C.WL("Part #:");
+                    String scan = C.getStringFinishableFunctions(out cancel, out finish, out fnKey);
+                    if (cancel)
+                    {
+                        var yn = C.YN("Are you sure? Existing.");
+                        cancel = yn;
+                        continue;
+
+                    }
+                    else if (finish)
+                    {
+                        var yn = C.YN("Are you sure? Existing.");
+                        finish = yn;
+                        continue;
+                    }
+                    else if (fnKey.HasValue)
+                    {
+
+                    }
+                    else if (!String.IsNullOrWhiteSpace(scan))
+                    {
+                        var part = ic.Find(scan);
+
+                        if (part != null)
+                        {
+                            partNo = part.PARTNUMBER;
+                        }
+                        else
+                        {
+                            C.A("Part# Not Found");
+                        }
+                    }
+                    else
+                    {
+                        C.A("Scan Error");
+                    }
+                }
+                if (location != null && partNo != null)
+                {
+                    ConsoleKey? fnKey;
+                    C.WL("Qty :");
+                    String scan = C.getStringFinishableFunctions(out cancel, out finish, out fnKey);
+                    if (cancel)
+                    {
+                        var yn = C.YN("Are you sure? Existing.");
+                        cancel = yn;
+                        continue;
+                    }
+                    else if (finish)
+                    {
+                        var yn = C.YN("Are you sure? Existing.");
+                        finish = yn;
+                        continue;
+                    }
+                    else if (fnKey.HasValue)
+                    {
+
+                    }
+                    else if (!String.IsNullOrWhiteSpace(scan))
+                    {
+                        var QTY = scan;
+                        if (QTY != null)
+                        {
+                            qty = Int32.Parse(QTY);
+                            isReady = true;
+                        }
+                        else
+                        {
+                            C.A("Invalid Qty");
+                        }
+                    }
+                    else
+                    {
+                        C.A("Entry Error");
+                    }
+                }
+                if (isReady)
+                {
+                    PartGetRsType rq = api.getPart(partNo);
+                    if (rq.statusCode == "1000")
+                    {
+                        item.PartNumber = partNo;
+                        item.Location = location.Name;
+                        item.Qty = Convert.ToString(qty);
+                        item.PartDescription = rq.Part.Description;
+                        item.UOM = rq.Part.UOM.Name;
+                        SaveToCSV(item);
+                        location = null;
+                        partNo = null;
+                        qty = 0;
+                    }
+                }
+
+            }
+
+
+        }
+
+        private void SaveToCSV(CycleLineItem item)
+        {
+            String sw = item.PartNumber +"," + item.PartDescription + "," +item.Location + "," +item.Qty + "," + item.QtyCommitted + "," + item.UOM + "," + item.Date + "," + item.Note + "," + item.Customer + "," + item.TrackingLotNumber + "," + item.TrackingRevisionLevel + "," + item.TrackingExpirationDate;
+            string fileName = DateTime.Now.ToString("ddMMyyyy") + ".csv";
+            File.WriteAllText(fileName, sw);
+        }
     }
 }
---
+++ InternationalArrivals.Beep1/CycleLineItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternationalArrivals.Beep1
+{
+    class CycleLineItem
+    {
+        public String PartNumber { get; set; }
+        public String PartDescription { get; set; }
+        public String Location { get; set; }
+        public String Qty { get; set; }
+        public String QtyCommitted { get; set; }
+        public String UOM { get; set; }
+        public String Date { get; set; }
+        public String Note { get; set; }
+        public String Customer { get; set; }
+        public String TrackingLotNumber { get; set; }
+        public String TrackingRevisionLevel { get; set; }
+        public String TrackingExpirationDate { get; set; }
+    }
+}
